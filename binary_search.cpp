19e09201335ae8cc2fc5934d504c4fe2c231ce16
Siddhant Jadhav <sidbnjadhav@gmail.com>
2022-10-19 18:15:36 +0530

    push-19/10

---
+++ binary_search.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int *input, int n, int val)
+{
+    // Write your code here
+    int start = 0, end = n - 1;
+    int mid;
+    while (start <= end)
+    {
+        mid = (start + end) / 2;
+        if (input[mid] == val)
+        {
+            return mid;
+        }
+        else if (val < input[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+
+    int size;
+    cin >> size;
+    int *input = new int[size];
+
+    for (int i = 0; i < size; ++i)
+    {
+        cin >> input[i];
+    }
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int val;
+        cin >> val;
+        cout << binarySearch(input, size, val) << endl;
+    }
+
+    delete[] input;
+    return 0;
+}
